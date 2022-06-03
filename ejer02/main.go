4fadb5d97385f7d0e42a6c41e1c79170cdf9e294
Ernesto Miguel Rodríguez Rodríguez <ernestomr87@gmail.com>
2022-06-03 18:19:42 -0400

    ejer01

---
+++ ejer02/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var number int
+var text string
+var status bool
+
+func main() {
+	number2, number3, number4, number5 := 2, 3, 5, "Hola Mundo"
+
+	fmt.Println(number2)
+	fmt.Println(number3)
+	fmt.Println(number4)
+	fmt.Println(number5)
+
+}
