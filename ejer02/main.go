249689538d328c9b0d919a5f1bd8f7dbcee9bb95
Ernesto Miguel Rodríguez Rodríguez <ernestomr87@gmail.com>
2022-06-03 18:31:20 -0400

    ejer02

--- ejer02/main.go
+++ ejer02/main.go
@@ -1,17 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 var number int
 var text string
-var status bool
+var status bool = true
 
 func main() {
-	number2, number3, number4, number5 := 2, 3, 5, "Hola Mundo"
+	number2, number3, number4, number5, status := 2, 3, 5, "Hola Mundo", false
+
+	var currency float64 = 3.14
+
+	number2 = int(currency)
+	text = strconv.Itoa(int(currency))
 
 	fmt.Println(number2)
 	fmt.Println(number3)
 	fmt.Println(number4)
 	fmt.Println(number5)
+	fmt.Println(status)
 
+	ShowStatus()
+
+}
+
+func ShowStatus() {
+	fmt.Println(status)
 }
